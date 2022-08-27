7f5ae3d11c214c79daa474e781772fd52521442d
Ryan B. Amos <rbamos@princeton.edu>
2022-08-27 16:02:30 -0400

    Finished printouts, basic proof of concept should work with a few tweaks

--- chrome/TabConverter.js
+++ chrome/TabConverter.js
@@ -21,33 +21,61 @@ all_note_names = ["Ab","A","A#","ab","a","a#","Bb","B","B#","bb","b","b#","Cb","
 
 /* Properties of notes */
 const NoteProperty = {
-  palm_mute: 'Palm mute',
-  pinch_harmonic: 'Pinch harmonic',
-  natural_harmonic: 'Natural harmonic',
-  legato: 'Legato',
-  vibrato: 'Vibrato',
-  bend: 'Bend',
-  muted: 'Muted',
-  staccato: 'Staccato',
-  dead_note: 'Dead note',
-  tied: 'Tied',
-  let_ring: 'Let ring',
-  up_strum: 'Up strum',
-  down_strum: 'Down strum',
-  accented: 'Accented',
-  heavily_accented: 'Heavily accented',
-  tapped: 'Tapped',
-  tremolo: 'Tremolo',
-  ghost_note: 'Ghost_note',
-  slide_up: "Slide up",
-  slide_down: "Slide down",
-  slide_up_into: "Slide up into", //slide when there is no prior note
-  slide_down_into: "Slide down into"
+  palm_mute: 'palm mute',
+  pinch_harmonic: 'pinch harmonic',
+  natural_harmonic: 'natural harmonic',
+  legato: 'legato',
+  vibrato: 'vibrato',
+  bend: 'bend',
+  release: 'release',
+  muted: 'muted',
+  tied: 'tied',
+  tapped: 'tap',
+  tremolo: 'tremolo',
+  ghost_note: 'ghost_note',
+  slide_up: "slide off up",
+  slide_down: "slide off down",
+  slide_up_into: "slide up", //slide when there is no prior note
+  slide_down_into: "slide down"
+}
+
+//e.g. palm mute on 6th string 2nd fret
+const NotePropertyBeforeOn = new Set([
+	NoteProperty.palm_mute, NoteProperty.pinch_harmonic, NoteProperty.natural_harmonic, NoteProperty.tapped, NoteProperty.tied, NoteProperty.ghost_note
+]);
+
+//e.g. Slide up 6th string 2nd fret
+const NotePropertyBeforeInto = new Set([
+	NoteProperty.slide_up_into, NoteProperty.slide_down_into
+]);
+
+//e.g. 6th string 2nd fret with vibrato
+const NotePropertyAfterWith = new Set([
+	NoteProperty.vibrato, NoteProperty.bend, NoteProperty.release, NoteProperty.tremolo
+]);
+
+//e.g. 6th string 2nd fret then legato
+const NotePropertyAfterThen = new Set([
+	NoteProperty.legato, NoteProperty.slide_up, NoteProperty.slide_down
+]);
+
+
+const ChordProperty = {
+	let_ring: 'Let ring',
+	up_strum: 'Up strum',
+	down_strum: 'Down strum',
+	accented: 'Accented',
+	heavily_accented: 'Heavily accented',
+	staccato: 'Staccato',
 }
 
 class Bend {
 	constructor(steps) {
 		this.steps = steps;
+	}
+
+	toString = function() {
+		
 	}
 }
 
@@ -57,6 +85,10 @@ class Duration {
 		this.duration = duration;
 		this.triplet = triplet;
 	}
+
+	toString = function() {
+		
+	}
 }
 
 class Pitch {
@@ -64,6 +96,15 @@ class Pitch {
 	constructor(fret,string) {
 		this.fret = fret;
 		this.string = string;
+	}
+
+	toString = function() {
+		if(this.fret == null) {
+			return `mute ${numberAdjective(this.string)} string`
+		} else if (this.fret == 0){
+			return `${numberAdjective(this.string)} string open`
+		}
+		return `${numberAdjective(this.string)} string ${numberAdjective(this.fret)} fret`
 	}
 	
 }
@@ -81,6 +122,35 @@ class Note {
 	addProperty = function(p) {
 		this.properties.push(p);
 	}
+
+	toString = function(count=null) {
+		//Todo implement version with count
+		let s = this.pitch.toString();
+
+		let before_on = listToText(this.properties.filter(value => NotePropertyBeforeOn.has(value)));
+		if(before_on != null){
+			s = `${before_on} on ${s}`;
+		}
+
+		let before_into = listToText(this.properties.filter(value => NotePropertyBeforeInto.has(value)));
+		if(before_into != null){
+			s = `${before_into} into ${s}`;
+		}
+
+		let after_with = listToText(this.properties.filter(value => NotePropertyAfterWith.has(value)));
+		if(after_with != null){
+			s = `${s} with ${after_with}`;
+		}
+
+		let after_then = this.properties.filter(value => NotePropertyAfterThen.has(value));
+		if(after_then != null){
+			after_then.forEach((val, idx, ar) => { //Different approach for this text
+				s = `${s} then ${val}`;
+			});
+		}
+
+		return `${s[0].toUpperCase()}${s.slice(1)}.`;
+	}
 }
 
 /* Chord */
@@ -88,36 +158,71 @@ class Chord {
 	constructor(notes) {
 		this.notes = notes;
 	}
+
+	toString = function() {
+		
+		let s = stringAndJoin(this.notes," ");//this.notes.forEach(val => val.toString()).join(" ");
+		return `<p>${s}<p>`;
+	}
 }
 
 class Comment {
 	constructor(text) {
 		this.text = text;
 	}
+
+	toString = function() {
+		match = /^\s*\[(.*?)\]\s*$/.exec(this.text)
+		if(match != null) {
+			return `<h2>${match[1]}</h2>`;
+		}
+		return `<p>${this.text}</p>`;
+	}
 }
 
 class Measure {
 	// Set fret = null for rest
-	constructor(notes) {
-		this.notes = notes
+	constructor(chords,measure_number) {
+		this.chords = chords
+		this.measure_number = measure_number;
 		this.bpm = null;
 		this.time_signature = null; //pair of integers
 		this.start_repeat = false;
 		this.end_repeat = false;
 		this.alernate_ending = null; //Number alternate ending
 	}
+
+	toString = function() {
+		//Todo: implement alternate endings etc
+		return `<h4>Measure ${this.measure_number}</h4>\n${stringAndJoin(this.chords,"\n")}`;
+	}
 }
 
 class Line {
-	constructor(measures){
+	constructor(measures,line_number){
 		this.measures = measures;
+		this.line_number = line_number;
+	}
+
+	toString = function() {
+		return `<h3>Line ${this.line_number}</h3>\n${stringAndJoin(this.measures,"\n")}`;
 	}
 }
 
 class Tab {
-	constructor(measures) {
-		this.measures = measures; //Can be a measure or a comment
+	constructor(lines) {
+		this.lines = lines; //Can be a measure or a comment
 		this.tuning = null;
+	}
+
+	toString = function() {
+		let s = "<h1>Tab</h1>";
+		let line_count = 1;
+		s += "<h2>Preamble</h2>"
+		this.lines.forEach((val,index,array) => {
+			s += `${val.toString()}`;
+		});
+		return s;
 	}
 }
 
@@ -128,6 +233,9 @@ class Tab {
  * Parsing
  */
 function parse_ultimate_guitar(root) {
+	let line_number = 1;
+	let measure_number = 1;
+
 	let music_lines = [];
 	let tab_container = $("code > pre",root);
 	console.log(tab_container.children());
@@ -135,7 +243,6 @@ function parse_ultimate_guitar(root) {
 		console.log(idx);
 		console.log(element);
 		element = $(element);
-		let measure = null;
 		switch(element.attr("class")){
 			case class_comment:
 				if(!rtrim(element.text())){
@@ -197,7 +304,7 @@ function parse_ultimate_guitar(root) {
 				let flush_measure = false;
 				let can_flush_notes = false;
 				let have_note = false;
-				let measure = new Measure([]);
+				let measure = new Measure([],measure_number++);
 				let line = []; //The line is a list of measures
 				for(let col = notes_start; col < note_line_length; col++) {
 
@@ -220,7 +327,7 @@ function parse_ultimate_guitar(root) {
 					}
 
 					if(flush_notes) {
-						measure.notes.push(parse_notes(note_texts));
+						measure.chords.push(parse_chord(note_texts));
 						note_texts = note_rows.map(row_text => "");
 						have_note = false;
 					}
@@ -245,35 +352,45 @@ function parse_ultimate_guitar(root) {
 					}
 					
 					if(flush_measure) {
+						//Check if we have remaining notes
+						if(note_texts.some(function(element, index, ar){return element != ""})){
+							measure.chords.push(parse_chord(note_texts));
+							note_texts = note_rows.map(row_text => "");
+							have_note = false;
+						}
 						line.push(measure);
-						measure = new Measure([]);
+						measure = new Measure([],measure_number++);
 					}
 				}
 
 				//Finish/flush incomplete reads
 				if(note_texts.some(function(element, index, ar){return element != ""})){
-					measure.notes.push(parse_notes(note_texts));
+					measure.chords.push(parse_chord(note_texts));
 				}
-				if(measure.notes.length != 0){
+				if(measure.chords.length != 0){
 					line.push(measure);
 				}
 
 				console.log(line);
 
-				music_lines.push(new Line(line));
+				music_lines.push(new Line(line,line_number++));
 
-				console.log(lines);
+				//console.log(lines);
 
 				console.log("Line length: " + line_length);
 
 				break;
 		}
 	});
-	console.log(music_lines);
+	//console.log(music_lines);
+	tab = new Tab(music_lines);
+	$("section.OnD3d.kmZt1").html(`${tab.toString()}\n${$("section.OnD3d.kmZt1").html()}`);
+	//console.log(tab.toString());
+	return tab;
 }
 //Note texts should be an array of strings
 //Returns a chord with the notes
-function parse_notes(note_texts) {
+function parse_chord(note_texts) {
 	notes = [];
 	for(let row = 0; row < note_texts.length; row++) {
 		
@@ -290,8 +407,9 @@ function parse_notes(note_texts) {
 		 * 5: Slide out
 		 * 6: legato
 		 * 7: Bend
+		 * 8: Release bend
 		 */
-		match = /o?([\\\/]?)([\(\[]?)(\d+|x)([\)\]]?)(~+)?([\\\/s]?)([hp]?)(b?)/.exec(note_texts[row]);
+		match = /o?([\\\/]?)([\(\[]?)(\d+|x)([\)\]]?)(~*)([\\\/s]?)([hp]?)(b?)(r?)/.exec(note_texts[row]);
 		if(!match) {
 			console.log("Failed to parse note text");
 			console.log(note_texts);
@@ -340,6 +458,7 @@ function parse_notes(note_texts) {
 		}
 		switch(groups[4]){
 			case "": //No match
+			case null:
 				break;
 			default: //Any number of tildes
 				note.addProperty(NoteProperty.vibrato);
@@ -364,14 +483,14 @@ function parse_notes(note_texts) {
 				note.addProperty(NoteProperty.bend);
 				break;
 		}
+		switch(groups[8]){
+			case "r":
+				note.addProperty(NoteProperty.release);
+				break;
+		}
 		notes.push(note);
 	}
 	return new Chord(notes);
-}
-
-function parse_page() {
-	console.log("Parse page");
-	parse_ultimate_guitar($("body"));
 }
 
 //Utilities
@@ -381,7 +500,46 @@ function rtrim(x) {
 	// This implementation removes whitespace from the right side
 	// of the input string.
 	return x.replace(/\s+$/gm, '');
-  }
+}
+//----
+
+//List of strings -> text list (warning: includes oxford comma)
+function listToText(l, joiner=','){
+	switch(l.length) {
+		case 0:
+			return null;
+		case 1:
+			return l[0];
+		case 2:
+			return `${l[0]} and ${l[1]}`;
+		default:
+			return `${l.slice(0,l.length-1).join(`${joiner} `)}${joiner} and ${l[l.length-1]}`;
+	}
+}
+
+function stringAndJoin(l,joiner=", ") {
+	//FIXME surely there must be a cleaner way to do this?
+	let str_list = [];
+	l.forEach((v) => {str_list.push(v.toString())});
+	return str_list.join(joiner);
+}
+
+
+//https://stackoverflow.com/a/20426113/582136
+var special = ['zeroth','first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth'];
+var deca = ['twent', 'thirt', 'fort', 'fift', 'sixt', 'sevent', 'eight', 'ninet'];
+
+function numberAdjective(n) {
+  if (n < 20) return special[n];
+  if (n%10 === 0) return deca[Math.floor(n/10)-2] + 'ieth';
+  return deca[Math.floor(n/10)-2] + 'y-' + special[n%10];
+}
+//----
+
+function parse_page() {
+	console.log("Parse page");
+	parse_ultimate_guitar($("body"));
+}
 
 console.log("TabConverter");
 
